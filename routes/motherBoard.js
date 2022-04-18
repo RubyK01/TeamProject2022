df0a98c2d9d6505a370cfeefd74b560410b1cd69
TomasK01 <tomaskehoe4@gmail.com>
2022-04-18 19:36:04 +0100

    mother board change

--- routes/motherBoard.js
+++ routes/motherBoard.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('cooling', { title: 'Cooling' });
+  res.render('motherBoard', { title: 'motherBoard' });
 });
 
 module.exports = router;
