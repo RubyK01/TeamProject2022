683782db22922b9145b9453171ec98f7f2d4834d
Your Name <glenwilliams835@yahoo.com>
2022-04-19 18:01:09 +0100

    updated

--- routes/cart.js
+++ routes/cart.js
@@ -6,9 +6,9 @@ router.get('/', function(req, res, next) {
   //login checker by Tomas.
   //If the user is not logged into an account while trying to go to this page,
   //they will be taken back to the login page with the follow error message.
-  if(!req.session.loggedIn === true){
-    res.redirect("/login"+"?&error=Please login to view page!");
-  }
+  //if(!req.session.loggedIn === true){
+    //res.redirect("/login"+"?&error=Please login to view page!");
+ // }
   res.render('cart', { title: 'cart' });
 });
 
