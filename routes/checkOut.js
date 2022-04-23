25cbedc7eb2dc534e8b08626134c18b6111198c3
jamiebrown39 <brownjamie1603@gmail.com>
2022-04-23 12:53:00 +0100

    payment push

--- routes/checkOut.js
+++ routes/checkOut.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var customerID = req.session.userID
+var session = require('express-session');
+var mysql = require('mysql');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,6 +13,26 @@ router.get('/', function(req, res, next) {
     res.redirect("/login"+"?&error=Please login to view page!");
   }
   res.render('checkOut', { title: 'cart' });
+});
+// the routes were done by thomas
+router.post('/Auth', async function(req, res, next) {
+	var fName = req.body.fName;
+	var lName = req.body.lName
+	var cardNumber = req.body.cardNumber;
+	var expiration = req.body.expiration;
+	var CVV = req.body.CVV;
+	var customerID = req.session.userID;
+	var connection = mysql.createConnection({
+    host: connection_details.host,
+    user: connection_details.user,
+    password: connection_details.password,
+    database: connection_details.database
+  });
+	connection.query("INSERT INTO payment(cardNum, fName, lName, secruityNum, expirayDate, customerID) VALUES (?,?,?,?,?,?));
+	if(){
+		var checkEmail = results[0].email;
+      var checkUsername = results[0].userName;
+	}
 });
 
 module.exports = router;
