04cda12b8aae17001a6b514b14b1b49feeef67af
Your Name <glenwilliams835@yahoo.com>
2022-04-24 10:35:01 +0100

    updated

---
+++ test/CartTest.js
@@ -0,0 +1,28 @@
+var generalProducts = require("../views/generalProducts.ejs");
+var assert = require("chai").assert;
+
+describe('generalProducts', function(){
+	it("Adding products to cart.", async function create(){
+			var addToCartButtons = true
+		if (addToCartButtons){
+			var cartItemContainer = true;
+		if (cartItemContainer){
+			var testResult = true;
+			}
+			else{
+		var testResult = false;
+		
+    }
+			
+		}
+		
+		
+
+	
+	 assert.equal(testResult,true)
+		
+    }
+	
+	
+		
+});
